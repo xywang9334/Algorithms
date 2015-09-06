d1a35fcd5df73021b7cb185f5c46e26025d43195
xywang9334 <wangmcindy@hotmail.com>
2015-09-06 17:27:33 -0500

    Update

--- pv.py
+++ pv.py
@@ -571,3 +571,510 @@ class Solution(object):
         self.invertTree(root.left)
         self.invertTree(root.right)
         return root
+
+
+# implement stack using queue
+class Stack(object):
+    def __init__(self):
+        """
+            initialize your data structure here.
+            """
+        self.l = list()
+        self.temp = list()
+    
+    
+    def push(self, x):
+        """
+            :type x: int
+            :rtype: nothing
+            """
+        self.l.append(x)
+    
+    
+    def pop(self):
+        """
+            :rtype: nothing
+            """
+        for num in self.l:
+            self.temp.append(num)
+        self.l = []
+        for num in self.temp[:-1]:
+            self.l.append(num)
+        self.temp = []
+    
+    
+    def top(self):
+        """
+            :rtype: int
+            """
+        for num in self.l[::-1]:
+            self.temp.append(num)
+        val = self.temp[0]
+        self.temp = []
+        return val
+    
+    
+    
+    def empty(self):
+        """
+            :rtype: bool
+            """
+        return self.l == []
+
+
+# Find the total area covered by two rectilinear rectangles in a 2D plane.
+class Solution(object):
+    def computeArea(self, A, B, C, D, E, F, G, H):
+        """
+            :type A: int
+            :type B: int
+            :type C: int
+            :type D: int
+            :type E: int
+            :type F: int
+            :type G: int
+            :type H: int
+            :rtype: int
+            """
+        
+        area_total = abs((C - A) * (D - B)) + abs((G - E) * (F - H))
+        overlap = max((min(C, G) - max(A, E)), 0) * max((min(D, H) - max(B, F)), 0)
+        return area_total - overlap
+
+
+
+
+# returns true if a list contains duplicates
+# Otherwise, returns false
+
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+            :type nums: List[int]
+            :rtype: bool
+            """
+        d = dict()
+        for num in nums:
+            d[num] = d.get(num, 0) + 1
+            if d[num] > 1:
+                return True
+        return False
+
+
+
+
+# Count the number of trailing zero in n!
+
+class Solution(object):
+    def trailingZeroes(self, n):
+        """
+            :type n: int
+            :rtype: int
+            """
+        
+        # count the number of 5s in the factorization
+        if n == 0:
+            return 0
+        return n / 5 + self.trailingZeroes(n / 5)
+
+#Given an array of integers and an integer k,
+#find out whether there are two distinct indices i and j in the array such that nums[i] = nums[j] and the difference between i and j is at most k.
+class Solution(object):
+    def containsNearbyDuplicate(self, nums, k):
+        """
+            :type nums: List[int]
+            :type k: int
+            :rtype: bool
+            """
+        d = dict()
+        for k1, v in list(enumerate(nums)):
+            if v in d and k1 - d[v] <= k:
+                return True
+            d[v] = k1
+        return False
+
+# reverse an integer
+class Solution(object):
+    def reverse(self, x):
+        """
+            :type x: int
+            :rtype: int
+            """
+        is_negative = False
+        if x < 0:
+            x = -x
+            is_negative = True
+        n = 0
+        length = len(str(x))
+        while x != 0:
+            n += (x % 10) * (10 ** (length - 1))
+            length -= 1
+            x /= 10
+        
+        # check overflows
+        if n > 2147483647:
+            return 0
+        if is_negative:
+            n = -n
+        return n
+
+# implement atoi
+
+class Solution(object):
+    def myAtoi(self, string):
+        """
+            :type str: str
+            :rtype: int
+            """
+        
+        string = string.strip()
+        integer = ""
+        first_occur = False
+        for char in string:
+            if char not in set(['+','-']) and (not str(char).isdigit()) and not first_occur:
+                break
+            if (not str(char).isdigit()) and first_occur:
+                break
+            if char == '+' and not first_occur:
+                first_occur = True
+            elif char == '-' and not first_occur:
+                first_occur = True
+                integer += "-"
+            elif str(char).isdigit():
+                integer += char
+                first_occur = True
+    
+        if integer == "" or integer == "-" or integer == "+":
+            return 0
+
+        i = int(integer)
+        if i > 2147483647:
+            return 2147483647
+        elif i < -2147483648:
+            return -2147483648
+        else:
+            return i
+
+# know if digit is a palindrome
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+            :type x: int
+            :rtype: bool
+            """
+        if x < 0:
+            return False
+        length = len(str(x))
+        i = length
+        if length % 2:
+            length += 1
+        while i > 0:
+            if x % 10 != x / (10 ** (i - 1)):
+                return False
+            x -= (x % 10) * (10 ** (i - 1))
+            i -= 2
+            x /= 10
+        return True
+
+# reverse a linkedlist
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def reverseList(self, head):
+        """
+            :type head: ListNode
+            :rtype: ListNode
+            """
+        pointer = head
+        pre = None
+        new_head = None
+        while pointer != None:
+            temp = pointer.next
+            if temp == None:
+                new_head = pointer
+            pointer.next = pre
+            pre = pointer
+            pointer = temp
+        return new_head
+
+
+#
+#Given two strings s and t, determine if they are isomorphic.
+#
+#Two strings are isomorphic if the characters in s can be replaced to get t.
+#
+#All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character but a character may map to itself.
+#
+#For example,
+#Given "egg", "add", return true.
+#
+#Given "foo", "bar", return false.
+#
+#Given "paper", "title", return true.
+
+class Solution(object):
+    def isIsomorphic(self, s, t):
+        """
+            :type s: str
+            :type t: str
+            :rtype: bool
+            """
+        d_t = dict()
+        d_s = dict()
+        if len(s) != len(t):
+            return False
+        for x in xrange(len(s)):
+            if s[x] not in d_s:
+                d_s[s[x]] = [x]
+            if t[x] not in d_t:
+                d_t[t[x]] = [x]
+            if s[x] in d_s:
+                d_s[s[x]] += [x]
+            if t[x] in d_t:
+                d_t[t[x]] += [x]
+
+    return sorted(d_t.values()) == sorted(d_s.values())
+
+# Count the number of prime numbers less than a non-negative number, n.
+class Solution(object):
+    def countPrimes(self, n):
+        """
+            :type n: int
+            :rtype: int
+            """
+        if n == 0 or n == 1:
+            return 0
+        
+        l = [True] * n
+        l[0] = l[1] = False
+        for i in xrange(2, n):
+            if l[i]:
+                for x in xrange(2, n / i + 1):
+                    if x * i < n:
+                        l[x * i] = False
+        return sum(l)
+
+# remove element by value in a linkedlist
+class Solution(object):
+    def removeElements(self, head, val):
+        """
+            :type head: ListNode
+            :type val: int
+            :rtype: ListNode
+            """
+        head2 = head
+        pre = head
+        pointer = head
+        
+        while pointer != None:
+            if pointer.val == val and pointer == head2:
+                pre = pointer
+                pointer = pointer.next
+                head2 = head2.next
+            elif pointer.val == val:
+                pre.next = pointer.next
+                pointer = pointer.next
+            else:
+                pre = pointer
+                pointer = pointer.next
+
+    return head2
+
+
+# if a number is a happy number
+
+class Solution(object):
+    def isHappy(self, n):
+        """
+            :type n: int
+            :rtype: bool
+            """
+        
+        sum = n
+        d = []
+        
+        while sum != 1:
+            string = str(n)
+            sum = 0
+            for char in string:
+                sum += int(char) ** 2
+            n = sum
+            if n in d:
+                return False
+            d.append(n)
+        
+        if n == 1:
+            return True
+        return False
+
+# dp
+#You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them is that adjacent houses have security system connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
+#
+#Given a list of non-negative integers representing the amount of money of each house, determine the maximum amount of money you can rob tonight without alerting the police.
+
+class Solution(object):
+    def rob(self, nums):
+        """
+            :type nums: List[int]
+            :rtype: int
+            """
+        n = len(nums)
+        if n == 2:
+            return max(nums[0], nums[1])
+        if n == 1:
+            return nums[0]
+        if n == 0:
+            return 0
+    
+    
+        sum = [0] * n
+        
+        i = 2
+        
+        sum[0] = nums[0]
+        sum[1] = max(nums[0], nums[1])
+        
+        
+        while i < n:
+            sum[i] = max(sum[i - 1], sum[i - 2] + nums[i])
+            i += 1
+        
+    return sum[n - 1]
+
+
+# min stack, return the minimum element in O(1) time
+class MinStack(object):
+    def __init__(self):
+        """
+            initialize your data structure here.
+            """
+        self.l = list()
+        self.min_l = list()
+        self.l_length = 0
+        self.min_l_length = 0
+    
+    
+    def push(self, x):
+        """
+            :type x: int
+            :rtype: nothing
+            """
+        self.l.append(x)
+        self.l_length += 1
+        if not self.min_l:
+            self.min_l.append(x)
+            self.min_l_length += 1
+        elif x <= self.min_l[self.min_l_length - 1]:
+            self.min_l.append(x)
+            self.min_l_length += 1
+
+
+    def pop(self):
+        """
+        :rtype: nothing
+        """
+        k = self.l.pop()
+        self.l_length -= 1
+        if k == self.min_l[self.min_l_length - 1]:
+           self.min_l.pop()
+           self.min_l_length -= 1
+
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        return self.l[self.l_length - 1]
+        
+        
+    def getMin(self):
+        """
+            :rtype: int
+            """
+        return self.min_l[self.min_l_length - 1]
+
+
+# Compute the h-index for a scholar
+class Solution(object):
+    def hIndex(self, citations):
+        """
+            :type citations: List[int]
+            :rtype: int
+            """
+        
+        citations.sort(reverse = True)
+        length = len(citations)
+        h_index = 0
+        while h_index < length:
+            if h_index < citations[h_index]:
+                h_index += 1
+            else:
+                break
+        return h_index
+
+# roman to integer
+class Solution(object):
+    def romanToInt(self, s):
+        """
+            :type s: str
+            :rtype: int
+            """
+        d = {}
+        d['I'] = 1
+        d['V'] = 5
+        d['X'] = 10
+        d['L'] = 50
+        d['C'] = 100
+        d['D'] = 500
+        d['M'] = 1000
+        
+        length = len(s)
+        sum = 0
+        for i in xrange(length):
+            if i < length - 1 and d.get(s[i]) < d.get(s[i + 1]):
+                sum -= d.get(s[i])
+            else:
+                sum += d.get(s[i])
+        return sum
+
+# merge two sorted lists in place
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        """
+            :type nums1: List[int]
+            :type m: int
+            :type nums2: List[int]
+            :type n: int
+            :rtype: void Do not return anything, modify nums1 in-place instead.
+            """
+        j = 0
+        for i in xrange(m, m + n):
+            nums1[i] = nums2[j]
+            j += 1
+        nums1.sort()
+
+# calculate the minimum     roof to leave     depth
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def minDepth(self, root):
+        """
+            :type root: TreeNode
+            :rtype: int
+            """
+        if root == None:
+            return 0
+        
+        if not root.left or not root.right:
+            return max(self.minDepth(root.left), self.minDepth(root.right)) + 1
+        else:
+        return min(self.minDepth(root.left), self.minDepth(root.right)) + 1
